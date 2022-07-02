6bf12d9a33a9ee627fa03f792ec5b836ffa5bf28
M3t0r <github@m3t0r.de>
2022-07-02 21:03:06 +0200

    Add disk usage graph

--- src/components/disk.rs
+++ src/components/disk.rs
@@ -6,6 +6,7 @@ use std::path::Path;
 use embedded_graphics::{
     prelude::*,
     text::{Baseline, Text},
+    primitives::Line,
 };
 
 #[derive(Debug)]
@@ -40,13 +41,36 @@ impl Component for Disk {
     }
 
     fn draw(&self, drawable: &mut Drawer, offset: Point, _tick: u64) -> Result<(), Error> {
-        Text::with_baseline(
-            format!("{}/{}", GlancableSizesWithOrdersOfMagnitude::new(self.size - self.available, Base::Two), GlancableSizesWithOrdersOfMagnitude::new(self.size, Base::Two)).as_str(),
-            offset,
-            drawable.base_text_style.clone(),
-            Baseline::Top,
-        )
+        let bar_width = (Drawer::WIDTH as f32 * (1f32 - (self.available as f32 / self.size as f32))) as i32;
+
+        Line::new(
+            Point::new(0, 5) + offset,
+            Point::new(bar_width, 5) + offset
+        ).into_styled(drawable.base_primitive_style)
         .draw(&mut drawable.display)?;
+
+        Line::new(
+            Point::new(0, 2) + offset,
+            Point::new(0, 8) + offset
+        ).into_styled(drawable.base_primitive_style)
+        .draw(&mut drawable.display)?;
+
+        Line::new(
+            Point::new(Drawer::WIDTH.into(), 2) + offset,
+            Point::new(Drawer::WIDTH.into(), 8) + offset
+        ).into_styled(drawable.base_primitive_style)
+        .draw(&mut drawable.display)?;
+
+        let size_str = format!("{}", GlancableSizesWithOrdersOfMagnitude::new(self.size, Base::Ten));
+        let size_text = Text::with_baseline(
+            size_str.as_str(),
+            offset + Point::new(0,0),
+            drawable.base_text_style,
+            Baseline::Top,
+        );
+
+        size_text.translate(Point::new(24,0)).draw(&mut drawable.display)?;
+
         Ok(())
     }
 }
